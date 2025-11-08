49024718a81808c37c9908c4e821693baaf5df78
Anders Magnusson <ragge@tethuvudet.se>
2025-11-08 18:18:47 +0100

    Add sanity check from Issue #38.

--- regress/c99/init006.c
+++ regress/c99/init006.c
@@ -1,5 +1,7 @@
 /* correct - from c99 */
 
+enum { foo = 42 }; struct { char *b; } x[] = { { (char []){ foo, 0, 0 } } };
+
 int main(int argc, char *argv[])
 {
 	/* pa[2] = {1,2} */ 
@@ -7,6 +9,14 @@ int main(int argc, char *argv[])
 	
 	/* pb[2] = {*pa, 0} */ 
 	int *pb = (int [2]){*pa};
+
+	/* self-referencing declarations */
+	void *list[] = {
+		&list[0],
+		&list[1]
+	};
+	if (&list[0] != list[0] || &list[1] != list[1])
+		return 1;
 
 	return 0; 
 }
